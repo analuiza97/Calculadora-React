8a52242fc83d9b3891c496a8511b1f9c1b9c6381
Ana Luíza Fontes <analuizaffontes97@gmail.com>
2020-08-03 17:42:35 -0300

    Alteração do título

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <div>
-        <h1>Minha Calculadora!</h1>
+        <h1>Calculadora</h1>
         <Calculator />
     </div>
 , document.getElementById('root'));
